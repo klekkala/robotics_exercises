c234831412ce4e86073a512c78d8ce241d8bc801
kiran4399 <kiran4399@gmail.com>
2016-10-12 21:42:03 +0530

    skew matrix function from linear triangualtion removed

--- robot_perception/Assignment4/LinearPnP.m
+++ robot_perception/Assignment4/LinearPnP.m
@@ -10,49 +10,31 @@ function [C, R] = LinearPnP(X, x, K)
 %     R - size (3 x 3) pose rotation
 
 
-numpts = size(x,1);
-if isempty(X) || isempty(x)
-    disp('problem');
+%converting to homogenious system
+X = [X , ones(size(X,1),1)];
+A = zeros(3 * size(X,1), 12);
+
+for i = 1 : size(X,1)
+    Xt = X(i,:);
+    x_temp = [x(i,:),1];
+    xx = K \ x_temp';    
+    j = (i-1)*3 + 1;
+    A(j:j+2,:) = [0, -1, xx(2); 1, 0, -xx(1); -xx(2), xx(1), 0] * ...
+                 [Xt, zeros(1,4), zeros(1,4); zeros(1,4), Xt, zeros(1,4); ...
+                  zeros(1,4), zeros(1,4), Xt];
 end
+[u,d,v] = svd(A);
+P_temp = v(:,end);
+P = reshape(P_temp, 4, 3)';
 
 
-if size(x,2) == 2
-    x = [x, ones(size(x,1),1)];
+R = P(:,1:3);
+[uu,dd,vv] = svd(R);
+if det(uu * vv') > 0
+    R = uu * vv';
+    T = P(:,4) ./ dd(1,1);
+else
+    R = -uu * vv';
+    T = -P(:,4) ./ dd(1,1);
 end
-
-
-if size(X,2) == 3
-    X = [X, ones(size(X,1),1)];
-end
-
-vec2skew = @(v) [0, -v(3), v(2); v(3), 0, -v(1); -v(2), v(1), 0];
-
-Xdiag = zeros(3,12);
-A = [];
-for i=1:numpts
-    Xdiag(1, 1:4) = X(i,:);
-    Xdiag(2, 5:8) = X(i,:);
-    Xdiag(3, 9:12) = X(i,:);
-    skew = vec2skew(inv(K)*x(i,:)');
-    A = [A; skew*Xdiag];
-
-end
-
-%least squares to solve for P
-[~,~,V] = svd(A);
-Pvec = V(:,end);
-P = [Pvec(1:4)';Pvec(5:8)';Pvec(9:12)'];
-
-% check
-check = zeros(3, numpts);
-for i = 1:numpts
-    check(:, i) = vec2skew(inv(K)*x(i,:)')*P*X(i,:)';
-end
-
-%reconditioning
-RC = P;
-[U,D,V] = svd(RC(:,1:3));
-R = sign(det(U*V'))*U*V';
-C = -R'*RC(:,4)/D(1,1);
-
-end
+%C = -R' * T;
